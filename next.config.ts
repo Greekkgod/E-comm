23166a486b42dbca0081c39245b90676b92a32df
Greekkgod <pradhumyagaur2003@gmail.com>
2026-01-21 18:11:26 +0530

    Optimize build: Skip TS checks and config images

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,17 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
